47920f9a0a0ddd448f006cbabad49568b0420ea9
nguHelon <nguhelon@gmail.com>
2022-10-30 19:31:13 +0100

    done with the responsiveness, Kinda

--- javascript/toggleCart.js
+++ javascript/toggleCart.js
@@ -1,6 +1,17 @@
 const openBtn = document.querySelector("#openCart");
 const closeBtn = document.querySelector(".closeCart");
 const cart = document.querySelector(".cart-section");
+const menuBtn = document.querySelector(".header .menu");
+const uldiv = document.querySelector(".header ul");
+const closeMenu = document.querySelector(".header ul .links i.fa-xmark");
+
+menuBtn.addEventListener("click", () => {
+    uldiv.classList.add("active");
+})
+
+closeMenu.addEventListener("click", () => {
+    uldiv.classList.remove("active");
+})
 
 openBtn.addEventListener("click", () => {
     cart.classList.add("active");
